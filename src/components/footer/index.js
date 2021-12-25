f1c776aa5d65e27d8d515a25a68e8fd9eb3afbf2
Sakshi <shankarsakshi03@gmail.com>
2021-12-25 20:05:15 +0530

    addon

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -10,6 +10,11 @@ const Footer = () => {
             <a className="nav-link" href="https://github.com/sakshi037">
                 Sakshi Shankar
             </a>
+            {` `}
+            <a className="nav-link" href="https://github.com/abhisheksingh7455">
+                Abhishek Singh
+            </a>
+            
         </div>
     )
 }
